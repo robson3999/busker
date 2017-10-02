1765d6f76b372fde4502522091eac1fcbd804003
Robert <robertglod104@gmail.com>
2017-10-02 14:07:40 +0200

    add songs feature added

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -10,6 +10,7 @@ class SongsController < ApplicationController
   # GET /songs/1
   # GET /songs/1.json
   def show
+    @song = Song.find(params[:id])
   end
 
   # GET /songs/new
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,10 @@
 class Song < ApplicationRecord
   belongs_to :book
+
+  # attr_accessor :title, :text, :author
+
+  def self.show_song(file)
+    file.readlines
+  end
+
 end
