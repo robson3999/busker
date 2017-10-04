fab87289731b88ecf5f362de4460b76981857261
Robert <robertglod104@gmail.com>
2017-10-04 12:17:27 +0200

    basic validation added, style improvements added, admin role added

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,8 +1,10 @@
 class Book < ApplicationRecord
   has_and_belongs_to_many :songs
   belongs_to :user
+  validates :title, presence: true
+  validates :title, length: {maximum: 30}
 
-  # Maybe implemented in future 
+  # Maybe implemented in future
   # def self.has_song?(id)
   #
   # end
