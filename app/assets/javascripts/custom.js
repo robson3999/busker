b050dbc66e1c7eb9ea852434535ed953f304f2e2
Robert <robertglod104@gmail.com>
2017-10-05 10:00:32 +0200

    precompiled assets for heroku

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,3 +1,16 @@
+/* Different font-sizes for song view */
+function smallestFont() {
+  document.getElementById("song-text-area").style.fontSize = "16px";
+};
+function mediumFont() {
+  document.getElementById("song-text-area").style.fontSize = "20px";
+};
+function largeFont() {
+  document.getElementById("song-text-area").style.fontSize = "xx-large";
+};
+
+
+
 document.addEventListener("turbolinks:load", function() {
 
 /* Text editor instead of text-area tag */
@@ -9,12 +22,3 @@ ClassicEditor
           console.error( error );
       } );
 });
-function smallestFont() {
-  document.getElementById("song-text-area").style.fontSize = "16px";
-};
-function mediumFont() {
-    document.getElementById("song-text-area").style.fontSize = "20px";
-};
-function largeFont() {
-    document.getElementById("song-text-area").style.fontSize = "xx-large";
-};
